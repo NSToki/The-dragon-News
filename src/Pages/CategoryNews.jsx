f3dce44309c870b68d307a53f79aae241fedf79a
NSToki <mdnstoki@gmail.com>
2025-11-24 14:36:23 +0600

    # Load News Data & Filter News Data based on Category

--- src/Pages/CategoryNews.jsx
+++ src/Pages/CategoryNews.jsx
@@ -1,13 +1,33 @@
-import React from 'react';
-import { useParams } from 'react-router';
+import React, { useEffect, useState } from "react";
+import { useLoaderData, useParams } from "react-router";
 
 const CategoryNews = () => {
-    const {id} = useParams();
-    return (
-        <div>
-           <h1>Category News:{id}</h1>
-        </div>
-    );
+  const { id } = useParams();
+  const data = useLoaderData();
+
+  const [categoryNews, setCategoryNews] = useState([]);
+
+  useEffect(() => {
+    if (id == "0") {
+      setCategoryNews(data);
+      return;
+    } else if (id == "1") {
+      const filterdNews = data.filter(
+        (news) => news.others.is_today_pick == true
+      );
+      setCategoryNews(filterdNews);
+    } else {
+      const filterdNews = data.filter((news) => news.category_id == id);
+      console.log(filterdNews);
+      setCategoryNews(filterdNews);
+    }
+  }, [data, id]);
+
+  return (
+    <div>
+      <h1>Category News found:{categoryNews.length}</h1>
+    </div>
+  );
 };
 
 export default CategoryNews;
