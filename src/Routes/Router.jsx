fd1fcf8149eafb70e284d5cf3366e6de2ecde4d3
NSToki <mdnstoki@gmail.com>
2025-11-24 14:33:47 +0600

    Load News Data & Filter News Data based on Category

--- src/Routes/Router.jsx
+++ src/Routes/Router.jsx
@@ -7,34 +7,33 @@ import About from "../Pages/About";
 const router = createBrowserRouter([
   {
     path: "/",
-    Component:HomeLayout,
-    children:[
+    Component: HomeLayout,
+    children: [
       {
-        path:'',
-        Component:Home
+        path: "",
+        Component: Home,
       },
       {
-        path:'/category/:id',
-        Component:CategoryNews
-      }
-    ]
-  },
-
-  {
-    path: "/auth",
-    element: <h1>Auth Layout</h1>,
-  },
-  {
-    path: "/news",
-    element: <h1>News Layout</h1>,
-  },
-  {
-    path:'/about',
-    Component:About
-  },
-  {
-    path: "/*",
-    element: <h1>Error 404</h1>,
+        path: "/category/:id",
+        Component: CategoryNews,
+      },
+      {
+        path: "/auth",
+        element: <h1>Auth Layout</h1>,
+      },
+      {
+        path: "/news",
+        element: <h1>News Layout</h1>,
+      },
+      {
+        path: "/about",
+        Component: About,
+      },
+      {
+        path: "/*",
+        element: <h1>Error 404</h1>,
+      },
+    ],
   },
 ]);
 
